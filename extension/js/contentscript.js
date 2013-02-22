add75c2fdfe8145f30b521273bb159a36029c0f7
samdutton <samdutton@gmail.com>
2013-02-22 14:11:15 +0000

    First version on CWS

--- extension/js/contentscript.js
+++ extension/js/contentscript.js
@@ -1,9 +1,15 @@
+// popup sends a message to the current tab
+// script below checks for resized and single pixel images
+// then responds with details
+
 
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse){
 		var resizedImages = [];
+		var singlePixelImages = {};
+		var numSinglePixelImages = 0;
 		var numImgElements = 0;
-    if (request.type == "sendResizedImages") {
+    if (request.type === "sendResizedImages") {
 			var imgElements = document.querySelectorAll("img");
 			numImgElements = imgElements.length;
 			if (numImgElements == 0) {
@@ -17,17 +23,27 @@ chrome.extension.onMessage.addListener(
 				var height = img.height;
 				var naturalWidth = img.naturalWidth;
 				var naturalHeight = img.naturalHeight;
-				if (!(naturalWidth == 1 && naturalHeight == 1) && // ignore 1x1px images
-					(width != naturalWidth || height != naturalHeight)) {
+				// check for single pixel images
+				if (naturalWidth == 1 && naturalHeight == 1) {
+					img.style.border= "10px dotted yellow";
+					img.setAttribute("data-highlighted", "true");
+					img.title = "Single-pixel image " + fileName;
+					numSinglePixelImages += 1; // total number of times that single pixel images are used
+					singlePixelImages[src] = fileName; // unique single-pixel images
+				// check for resized images
+				} else if (width != naturalWidth || height != naturalHeight) {
 					img.style.border= "10px dotted red";
+					img.setAttribute("data-highlighted", "true");
 					var ratioMessage = "";
 					var naturalAspectRatio = (naturalWidth/naturalHeight).toFixed(3);
 					var aspectRatio = (width/height).toFixed(3);
+					// check for changed aspect ratio
 					if (naturalAspectRatio !== aspectRatio) {
 						ratioMessage = "Resized image has changed aspect ratio (width/height):" +
 							"\n- natural " + naturalAspectRatio +
 							"\n- resized " + aspectRatio;
 					}
+					// set title (hover message) for image on page
 					img.title = "Image has been resized:\n" +
 						"- natural " + naturalWidth + "x" + naturalHeight + "px\n" +
 						"- resized " + width + "x" + height + "px\n\n" + ratioMessage;
@@ -42,8 +58,23 @@ chrome.extension.onMessage.addListener(
 					});
 				}
 			}
-			if (resizedImages.length > 0) {
-				sendResponse({"type": "test", "resizedImages": resizedImages});
+			if (resizedImages.length > 0 || numSinglePixelImages > 0) {
+				sendResponse({
+					"resizedImages": resizedImages,
+					"singlePixelImages": singlePixelImages,
+					"numSinglePixelImages": numSinglePixelImages
+				});
+			}
+		} else if (request.type === "scrollImageIntoView"){
+			var src = request.src;
+			var imgElements = document.querySelectorAll("img");
+			for (var i = 0; i != imgElements.length; ++i){
+				var imgElement = imgElements[i];
+				// images may be used more than once, and not always resized
+				if (imgElement.src === src && imgElement.dataset.highlighted === "true"){
+					imgElement.scrollIntoViewIfNeeded(); // centres in viewport
+					imgElement.style.borderStyle = "solid";
+				}
 			}
 		}
 });
--- extension/js/popup.js
+++ extension/js/popup.js
@@ -8,32 +8,88 @@
 var noneFound = document.querySelector("#noneFound");
 var images = document.querySelector("#images");
 
+// handle clicks on single-pixel or resized images
+// 'this' is the div for the image, which has a data-src attribute
+function scrollImageIntoView(){
+	var src = this.dataset.src;
+	chrome.tabs.getSelected(null, function(tab) {
+	  chrome.tabs.sendMessage(tab.id, {
+		  	"type": "scrollImageIntoView",
+		  	"src": src
+	  }, function(response){});
+  });
+}
+
+// send a message to the current tab -- contentscript.js checks for
+// resized and single pixel images then responds with details
 chrome.tabs.getSelected(null, function(tab) {
   chrome.tabs.sendMessage(tab.id, {type: "sendResizedImages"}, function(response) {
-		noneFound.style.display = "none";
+//  	console.log("popup.js got response: ", response);
+		noneFound.style.display = "none"; // if there is a response, images have been found
+
+		var singlePixelImages = response.singlePixelImages;
+		var numUniqueSinglePixelImages = Object.keys(singlePixelImages).length; // unique images
+		var numSinglePixelImages = response.numSinglePixelImages; // total number of times images used
+		if (numSinglePixelImages > 0) {
+			var singlePixelContainer = document.createElement("div");
+			singlePixelContainer.setAttribute("title", "Click to scroll image into view");
+			singlePixelContainer.id = "singlePixelImages";
+			singlePixelContainer.innerHTML = "<h2>Found " +
+				numUniqueSinglePixelImages + " single-pixel image(s) used " +
+				numSinglePixelImages + " time(s):</h2>";
+			for (var src in singlePixelImages){
+				var singlePixelDiv = document.createElement("div");
+				singlePixelDiv.className = "singlePixelImage";
+				singlePixelDiv.setAttribute("data-src", src); // used by click handler
+				var singlePixelFileName = singlePixelImages[src]; // key is src, value is fileName
+				singlePixelDiv.innerHTML += singlePixelFileName;
+				singlePixelDiv.onclick = scrollImageIntoView;
+				singlePixelContainer.appendChild(singlePixelDiv);
+			}
+			images.appendChild(singlePixelContainer);
+		}
+
   	var resizedImages = response.resizedImages;
   	var numResized = resizedImages.length;
-  	console.log(numResized);
-		images.innerHTML += "<div id='numResized'>Found " + numResized + " resized images</div>"
-  	for (var i = 0; i != numResized; ++i){
-  		var image = resizedImages[i];
-  		var src = image.src;
-  		var fileName = image.fileName;
-  		var width = image.width;
-  		var height = image.height;
-  		var naturalWidth = image.naturalWidth;
-  		var naturalHeight = image.naturalHeight;
-  		var ratioMessage = "";
-  		if (width/height !== naturalWidth/naturalHeight) {
-  			ratioMessage = "<br /><br />Resized image has changed aspect ratio (width/height):" +
-  				"<br />- natural " + (naturalWidth/naturalHeight).toFixed(3) +
-  				"<br />- resized " + (width/height).toFixed(3);
-  		}
-  		var message = "<img class='thumbnail' src='" + src + "' />" +
-  			fileName + ":<br />" +
-  			"- natural " + naturalWidth + "x" + naturalHeight + "px<br />" +
-  			"- resized " + width + "x" + height + "px" + ratioMessage;
-  		images.innerHTML += "<div class='image'>" + message + "</div>";
-  	}
+  	if (numResized > 0) {
+			var resizedContainer = document.createElement("div");
+			resizedContainer.id = "resizedImages";
+			resizedContainer.setAttribute("title", "Click to scroll image into view");
+			resizedContainer.innerHTML = "<h2>Found " + numResized + " resized image(s)</h2>"
+			for (var i = 0; i != numResized; ++i){
+	  		var image = resizedImages[i];
+	  		var src = image.src;
+	  		var fileName = image.fileName;
+	  		var width = image.width;
+	  		var height = image.height;
+	  		var naturalWidth = image.naturalWidth;
+	  		var naturalHeight = image.naturalHeight;
+	  		var ratioMessage = "";
+				var naturalAspectRatio = (naturalWidth/naturalHeight).toFixed(3);
+				var aspectRatio = (width/height).toFixed(3);
+
+				// check for images with changed aspect ratio
+	  		if (naturalAspectRatio !== aspectRatio) {
+	  			ratioMessage = "<br />Resized image has changed aspect ratio (width/height):" +
+	  				"<br />- natural: " + (naturalWidth/naturalHeight).toFixed(3) +
+	  				"<br />- resized: " + (width/height).toFixed(3);
+	  		}
+
+	  		var message = "<img class='thumbnail' src='" + src + "' />" +
+	  			"<div class='resizedImageFilename'>" + fileName + "</div>" +
+	  			"- natural " + naturalWidth + "x" + naturalHeight + "px<br />" +
+	  			"- resized " + width + "x" + height + "px" + ratioMessage;
+
+				var resizedImageDiv = document.createElement("div");
+				resizedImageDiv.className = "resizedImage";
+				resizedImageDiv.setAttribute("data-src", src); // used by click handler
+				resizedImageDiv.innerHTML += message;
+				resizedImageDiv.onclick = scrollImageIntoView;
+				resizedContainer.appendChild(resizedImageDiv);
+			}
+	  	images.appendChild(resizedContainer);
+		} else {
+			images.innerHTML += "<div id='numResized'>No resized images found.</div>";
+		}
   });
 });
